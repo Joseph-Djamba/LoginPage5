69939b72c39d3809736d17f2a767d8b4c2bd152b
jeefumba <jeefrader2@gmail.com>
2021-03-30 13:12:24 +0200

    second change of Correct Project

---
+++ First Folder/Cput 3/ADP3/IntelliJ IDEA/src/ac/za/cput/fifth/sum.java
@@ -0,0 +1,14 @@
+package ac.za.cput.fifth;
+
+import java.sql.SQLOutput;
+
+public class sum {
+    public static void main (String [] args){
+        System.out.println("Hello");
+
+        System.out.println("Hello to th second world ");
+
+
+        // This is only in the second branch
+    }
+}
